fc5c924613203f3200affe42c4f142524289e48f
sumangalasipoy@gmail.com <sumangalasipoy@gmail.com>
2025-05-05 15:30:07 +0700

    Added search-student

--- Main.java
+++ Main.java
@@ -61,3 +61,25 @@ public class Main {
             return "ID: " + id + ", Name: " + name + ", Grade: " + grade; 
         } 
     } 
+
+    static void viewStudents() {
+        if (students.isEmpty()) {
+        System.out.println("No students found.");
+        return;
+        }
+        System.out.println("\n--- List of Students ---");
+        for (Student s : students) {
+        System.out.println(s);
+        }
+        }
+        static void searchStudent() {
+        System.out.print("Enter Student ID to search: ");
+        int id = sc.nextInt();
+        for (Student s : students) {
+        if (s.getId() == id) {
+        System.out.println("Student Found: " + s);
+        return;
+        }
+        }
+        System.out.println("Student with ID " + id + " not found.");
+    }        
